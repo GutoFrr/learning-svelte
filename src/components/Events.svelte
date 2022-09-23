9a151f8fcf2c259dd44e76dea6651eae0d56f3ee
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-23 16:25:24 -0300

    component event dispatch

--- src/components/Events.svelte
+++ src/components/Events.svelte
@@ -1,6 +1,11 @@
 <script>
+  import ComponentEvents from './events/ComponentEvents.svelte'
   import DomEvents from './events/DOMEvents.svelte'
   import EventModifiers from './events/EventModifiers.svelte'
+
+  const handleMessage = (event) => {
+    alert(event.detail.text)
+  }
 </script>
 
 <main class="flex flex-col gap-5">
@@ -11,5 +16,9 @@
   <div>
     <h2 class="section-title">Event Modifiers</h2>
     <EventModifiers />
+  </div>
+  <div>
+    <h2 class="section-title">Component Events</h2>
+    <ComponentEvents on:message={handleMessage} />
   </div>
 </main>
