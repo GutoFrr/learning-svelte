9f6cf77b793cf0084d57e082b972e3879e2c56f6
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-23 09:30:10 -0300

    inital commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Counter from './components/Counter.svelte'
+</script>
+
+<main>
+  <h1 class="text-4xl font-bold">Learning Svelte!</h1>
+  <Counter />
+</main>
+
+<style>
+</style>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,22 @@
+<script>
+  let count = 0
+  $: doubled = count * 2
+
+  const incrementCount = () => {
+    count += 1
+  }
+
+  const resetCount = () => {
+    count = 0
+  }
+</script>
+
+<main>
+  <h2 class="title">Counter</h2>
+  <button on:click={incrementCount} class="btn">
+    count is {count}
+  </button>
+  <button on:click={resetCount} class="btn"> reset count </button>
+
+  <p>{count} doubled is {doubled}</p>
+</main>
