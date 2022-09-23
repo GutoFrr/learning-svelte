57dabbe97a8fce8ee4613b3fbc09ce6b87f4d2ce
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-23 16:17:03 -0300

    event modifiers

--- src/components/Events.svelte
+++ src/components/Events.svelte
@@ -1,10 +1,15 @@
 <script>
   import DomEvents from './events/DOMEvents.svelte'
+  import EventModifiers from './events/EventModifiers.svelte'
 </script>
 
-<main class="overflow-hidden">
+<main class="flex flex-col gap-5">
   <div>
     <h2 class="section-title">DOM Events</h2>
     <DomEvents />
+  </div>
+  <div>
+    <h2 class="section-title">Event Modifiers</h2>
+    <EventModifiers />
   </div>
 </main>
---
+++ src/components/events/EventModifiers.svelte
@@ -0,0 +1,20 @@
+<script>
+  const handleClick = () => {
+    alert('no more alerts')
+  }
+</script>
+
+<button class="btn" on:click|once={handleClick}>click me</button>
+
+<!-- 
+events modifiers
+
+preventDefault — calls event.preventDefault() before running the handler. Useful for client-side form handling, for example.
+stopPropagation — calls event.stopPropagation(), preventing the event reaching the next element
+passive — improves scrolling performance on touch/wheel events (Svelte will add it automatically where it's safe to do so)
+nonpassive — explicitly set passive: false
+capture — fires the handler during the capture phase instead of the bubbling phase (MDN docs)
+once — remove the handler after the first time it runs
+self — only trigger handler if event.target is the element itself
+trusted — only trigger handler if event.isTrusted is true. I.e. if the event is triggered by a user action.
+ -->
