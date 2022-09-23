5f564102a8429c1c0d8b6c9f7261e84feca333a9
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-23 11:43:52 -0300

    else if block statement

--- src/components/Logic.svelte
+++ src/components/Logic.svelte
@@ -1,5 +1,15 @@
 <script>
+  import ElseIfBlocks from './logic/ElseIfBlocks.svelte'
   import IfBlock from './logic/IfBlock.svelte'
 </script>
 
-<IfBlock />
+<main class="flex flex-col gap-5">
+  <div>
+    <h2 class="section-title">If Statement block</h2>
+    <IfBlock />
+  </div>
+  <div>
+    <h2 class="section-title">Else If Statement block</h2>
+    <ElseIfBlocks />
+  </div>
+</main>
