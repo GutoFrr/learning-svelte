6f251f531c4e85157d3d4bfa5b7bb2974689ad2e
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-23 12:21:55 -0300

    keyed each blcoks

--- src/components/Logic.svelte
+++ src/components/Logic.svelte
@@ -2,19 +2,24 @@
   import EachBlocks from './logic/EachBlocks.svelte'
   import ElseIfBlocks from './logic/ElseIfBlocks.svelte'
   import IfBlock from './logic/IfBlocks.svelte'
+  import KeyedEachBlocks from './logic/KeyedEachBlocks.svelte'
 </script>
 
 <main class="flex flex-col gap-5">
   <div>
-    <h2 class="section-title">If Statement block</h2>
+    <h2 class="section-title">If Statement blocks</h2>
     <IfBlock />
   </div>
   <div>
-    <h2 class="section-title">Else If Statement block</h2>
+    <h2 class="section-title">Else If Statement blocks</h2>
     <ElseIfBlocks />
   </div>
   <div>
-    <h2 class="section-title">Each block</h2>
+    <h2 class="section-title">Each blocks</h2>
     <EachBlocks />
+  </div>
+  <div>
+    <h2 class="section-title">Keyed Each blocks</h2>
+    <KeyedEachBlocks />
   </div>
 </main>
---
+++ src/components/logic/KeyedEachBlocks.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Thing from './Thing.svelte'
+
+  let things = [
+    { id: 1, name: 'apple' },
+    { id: 2, name: 'banana' },
+    { id: 3, name: 'carrot' },
+    { id: 4, name: 'doughnut' },
+    { id: 5, name: 'egg' },
+  ]
+
+  const handleClick = () => {
+    things = things.slice(1)
+  }
+
+  const resetThings = () => {
+    things = [
+      { id: 1, name: 'apple' },
+      { id: 2, name: 'banana' },
+      { id: 3, name: 'carrot' },
+      { id: 4, name: 'doughnut' },
+      { id: 5, name: 'egg' },
+    ]
+  }
+</script>
+
+{#if things.length > 0}
+  <button on:click={handleClick} class="btn"> Remove first thing </button>
+{:else}
+  <button on:click={resetThings} class="btn"> Reset things </button>
+{/if}
+
+{#each things as thing (thing.id)}
+  <Thing name={thing.name} />
+{/each}
