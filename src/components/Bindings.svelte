5bb914d46c6009fef53d2b7d48982b3b328dc93b
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-25 14:49:53 -0300

    numeric inputs

--- src/components/Bindings.svelte
+++ src/components/Bindings.svelte
@@ -1,11 +1,15 @@
 <script>
-  import TextInputs from "./bindings/TextInputs.svelte"
-
+  import NumericInputs from './bindings/NumericInputs.svelte'
+  import TextInputs from './bindings/TextInputs.svelte'
 </script>
 
 <main class="flex flex-col gap-5">
   <div>
     <h2 class="section-title">Text Inputs</h2>
     <TextInputs />
+  </div>
+  <div>
+    <h2 class="section-title">Numeric Inputs</h2>
+    <NumericInputs />
   </div>
 </main>
---
+++ src/components/bindings/NumericInputs.svelte
@@ -0,0 +1,29 @@
+<script>
+  let a = 1
+  let b = 2
+  $: result = a + b
+</script>
+
+<label class="block mb-1">
+  <input
+    type="number"
+    bind:value={a}
+    min="0"
+    max="10"
+    class="border-svelte border-2 rounded p-1"
+  />
+  <input type="range" bind:value={a} min="0" max="10" />
+</label>
+
+<label>
+  <input
+    type="number"
+    bind:value={b}
+    min="0"
+    max="10"
+    class="border-svelte border-2 rounded p-1"
+  />
+  <input type="range" bind:value={b} min="0" max="10" />
+</label>
+
+<p>{a} + {b} = {result}</p>
