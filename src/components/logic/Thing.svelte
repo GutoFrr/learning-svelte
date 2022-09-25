177ee0f88ab24d9f29b219967a5475e52f5253ab
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-09-25 15:16:06 -0300

    tailwinded thing component

--- src/components/logic/Thing.svelte
+++ src/components/logic/Thing.svelte
@@ -14,22 +14,14 @@
   const emoji = emojis[name]
 </script>
 
-<p>
-  <span>
+<p class="my-3">
+  <span class="inline-block py-1 px-4 text-center rounded-sm">
     The emoji for {name} is {emoji}
   </span>
 </p>
 
 <style>
-  p {
-    margin: 0.8em 0;
-  }
-
   span {
-    display: inline-block;
-    padding: 0.2em 1em 0.3em;
-    text-align: center;
-    border-radius: 0.2em;
     background-color: #ffdfd3;
   }
 </style>
